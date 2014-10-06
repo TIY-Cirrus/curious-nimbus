be35d44cf82b1985b57d848178fbde220e275eb3
Robert Groller <robertgroller@gmail.com>
2014-10-06 11:44:48 -0400

    Switch to paperclip defaults

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,3 +1,3 @@
 # config/initializers/paperclip.rb
-Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
-Paperclip::Attachment.default_options[:path] = '/:class/:attachment/id_partition/:filename'
+#Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+#Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:style/id_partition/:filename'
